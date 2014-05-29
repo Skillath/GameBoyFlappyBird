a65031988ec819adab39ef8c62f6654940616009
Skillath <xabier.gonzalez.69@gmail.com>
2014-05-29 17:23:35 +0200

    Points are shown

--- header.h
+++ header.h
@@ -1,9 +1,25 @@
+#include <gb/gb.h>
+#include <stdio.h>
+#include <gb/drawing.h>
+#include <gb/console.h>
+#include "Bird.c"
+
 int xBird = 30;  
 int xBirdLow = xBird - 8;  
 int yBird = 78;
 int yTailBird = yBird - 8;
+int xUpperPlumb, xLowerPlumb;
 
+struct Plumb
+{
+	int xPlumb;
+	int heightPlumb;
+};
 
 void animateBird(int iter);
 void jumpBird();
-void setPlumb();
+void setPlumb(struct Plumb* plumb, int nPlumb);
+void addPoints(int* p);
+int collision(struct Plumb* plumb, int nPlumb);
+int countPoints(struct Plumb* plumb, int nPlumb, int *p);
+int rand(int lim);
--- main.c
+++ main.c
@@ -1,13 +1,35 @@
-#include <gb/gb.h>
-#include <stdio.h>
-#include <gb/console.h>
-#include "Bird.c"
+/*
+Game made by Xabier Gonzalez Goienetxea (@Skillath). Not designed by me.
+
+GAMEBOY SPECIFICATIONS:
+- CPU: Custom 8-bit Sharp LR35902 core at 4.19 MHz. 
+- RAM: 8 kB internal S-RAM (can be extended up to 32 kB)
+- Video RAM: 8 kB internal
+- ROM: On-CPU-Die 256-byte bootstrap; 256 kb, 512 kb, 1 Mb, 2 Mb, 4 Mb and 8 Mb cartridges
+- Sound: 2 pulse wave generators, 1 PCM 4-bit wave sample (64 4-bit samples played in 1×64 bank or 2×32 bank) channel, 1 noise generator, and one audio input from the cartridge.
+- Display: Reflective STN LCD 160 × 144 pixels
+- Frame Rate: Approx. 59.7 frames per second on a regular Game Boy, 61.1 on a Super Game Boy
+- Vertical Blank Rate: Approx 1.1 ms
+- Screen size: 66 mm (2.6 in) diagonal
+- Color Palette: 2-bit (4 shades of "gray")
+- Power: 6 V, 0.7 W (4 AA batteries provide 15–30+ hours)
+- Dimensions: 90 mm (W) × 148 mm (H) × 32 mm (D) / 3.5″ × 5.8″ × 1.3″ 
+*/
+
 #include "header.h"
 
 void main()
 {
+	struct Plumb plumb[3];
+	int points = 0;
 	int iteration = 0;
-	set_sprite_data(0, 14, Bird);
+	int cont;
+	int nPlumb = 0;
+
+	/*color(BLACK,LTGREY,SOLID);
+	box(0,130,40,143,M_NOFILL);
+    box(50,130,90,143,M_FILL);*/
+	set_sprite_data(0, 15, Bird);
 	set_sprite_tile(0, 0); //Parameter 1 = number of the tile for reference from other functions. Parameter 2: The number of the tile of the GBTD
   	set_sprite_tile(1, 1);
   	set_sprite_tile(2, 2);
@@ -18,11 +40,18 @@ void main()
 	move_sprite(1, xBird, yBird);
 	move_sprite(2, xBirdLow, yTailBird);
 	move_sprite(3, xBirdLow, yBird);
+	
+
+	for(cont = 0; cont < 3; cont ++)
+		plumb[cont].xPlumb = 160;
 
 	while (!0) 
 	{
+		setPlumb(plumb, nPlumb);
 		animateBird(iteration);
 		jumpBird();
+		addPoints(&points);
+
 		delay(50);
 		iteration++;
 		if(iteration >= 4)
@@ -37,28 +66,28 @@ void animateBird(int iter)
 	switch(iter)
 	{
 		case 0:
-			set_sprite_tile(2,4);
-			move_sprite(2,xBirdLow,yTailBird);
-			set_sprite_tile(3,5);
-			move_sprite(3,xBirdLow,yBird);
+			set_sprite_tile(2, 4);
+			move_sprite(2, xBirdLow, yTailBird);
+			set_sprite_tile(3, 5);
+			move_sprite(3, xBirdLow, yBird);
 			break;
 		case 1:
-			set_sprite_tile(2,2);
-			move_sprite(2,xBirdLow,yTailBird);
-			set_sprite_tile(3,3);
-			move_sprite(3,xBirdLow,yBird);
+			set_sprite_tile(2, 2);
+			move_sprite(2, xBirdLow, yTailBird);
+			set_sprite_tile(3, 3);
+			move_sprite(3, xBirdLow, yBird);
 			break;
 		case 2:
-			set_sprite_tile(2,4);
-			move_sprite(2,xBirdLow,yTailBird);
-			set_sprite_tile(3,5);
-			move_sprite(3,xBirdLow,yBird);
+			set_sprite_tile(2, 4);
+			move_sprite(2, xBirdLow, yTailBird);
+			set_sprite_tile(3, 5);
+			move_sprite(3, xBirdLow, yBird);
 			break;
 		default:
-			set_sprite_tile(2,6);
-			move_sprite(2,xBirdLow,yTailBird);
-			set_sprite_tile(3,7);
-			move_sprite(3,xBirdLow,yBird);
+			set_sprite_tile(2, 6);
+			move_sprite(2, xBirdLow, yTailBird);
+			set_sprite_tile(3, 7);
+			move_sprite(3, xBirdLow, yBird);
 			break;	
 	}
 }
@@ -76,10 +105,40 @@ void jumpBird()
 	{
 		yTailBird += 4;
 		yBird += 4;
-	}	
+	}
+	if(pad == J_START)	
+	{
+		waitpad(J_START);
+	}
 }
 
-void setPlumb()
+void setPlumb(struct Plumb* plumb, int nPlumb)
 {
+	int cont;
+	for(cont = 0; cont < nPlumb; cont ++)
+	{
+		if(plumb[cont].xPlumb == 0)
+			plumb[cont].xPlumb = 20;
+		if(plumb[cont].xPlumb == 20)
+			plumb[cont].heightPlumb = rand(18);
+		//set_sprite_tile(plumb[cont].xPlumb, 6);
+		//box(plumb[cont].xPlumb,plumb[cont].heightPlumb, plumb[cont].xPlumb + 1, 0,M_FILL);
+	}
+	return;
+}
 
+void addPoints(int* p)
+{
+	*p += 1;
+	gotoxy(0, 0);
+	printf("POINTS: %d", *p);
+}
+
+//Custom rand function.
+int rand(int lim)
+{
+	long a = 3;
+    a = (((a * 214013L + 2531011L) >> 16) & 32767);
+        
+    return ((a % lim) + 1);
 }
